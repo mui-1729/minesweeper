b4d5cc21f3c438198ceafc9ddebfd8eb8bf7d9ab
mui-1729 <s1f102503015@iniad.org>
2025-05-13 07:32:20 +0900

    関数

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,29 +14,38 @@ const directions = [
   [-1, 1],
 ];
 
-const generateBomb = (X:number, Y:number) => {
-  let bombPlace = 0
+// 初手マップ生成
+const generateBomb = (firstX: number, firstY: number) => {
+  const newMap: number[][] = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0));
+  let bombPlace = 0;
 
-  while (bombPlace <= 10)  {
-    const x = Math.floor(Math.random() * 9)
-    const y = Math.floor(Math.random() * 9)
+  while (bombPlace <= 10) {
+    const x = Math.floor(Math.random() * 9);
+    const y = Math.floor(Math.random() * 9);
 
-    if ((x !== X || y !== Y) && )
-  }
-}
-
-const countBoard = (x:number, y:number, bombMap:number[][]) => {
-  return directions.reduce((count, [dx, dy]) => {
-    const nx = x + dx, ny = y + dy;
-    if (nx >= 0 && nx < 9 && ny >= 0 && ny < 9 && bombMap[ny][nx] === 1) {
-      count++;
+    if ((x !== firstX || y !== firstY) && newMap[y][x] === 0) {
+      newMap[y][x] = 1;
+      bombPlace++;
     }
-    return count;
-  }, 0);
-}
+  }
+  return newMap;
+};
 
+// 周囲のボム確認
+const countBoardAround = (x: number, y: number, bombMap: number[][]) => {
+  return directions.reduce((bombCount, [dx, dy]) => {
+    const nx = x + dx,
+      ny = y + dy;
+    if (nx >= 0 && nx < 9 && ny >= 0 && ny < 9 && bombMap[ny][nx] === 1) {
+      bombCount + 1;
+    }
+    return bombCount;
+  }, 0);
+};
 
 export default function Home() {
+  const [gameOver, setGameOver] = useState(false);
+  const [isFirstClick, setIsFirstClick] = useState(true);
   const [userInputs, setUserInputs] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -60,27 +69,39 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const board = userInputs.map((row, y) => {
-    row.map((cell, x) => {
-      if (cell === 0) return;
-    });
-  });
+  const clickHandler = (x: number, y: number) => {
+    if (gameOver === true || userInputs[y][x] !== 0) return;
 
-  const
+    if (isFirstClick === true) {
+      const nextMap = generateBomb(x, y);
+      setBombMap(nextMap);
+      setIsFirstClick(false);
+      clickHandler(x, y);
+      return;
+    }
+    if (bombMap[y][x] === 1) {
+      alert('ゲームオーバー');
+      setGameOver(true);
+      return;
+    }
 
-  // const clickHandler = () => {
-  //   setuserInputs();
-  //   setbombMap();
-  // };
+    const count = countBoardAround(x, y, bombMap);
+    const newInputs = userInputs.map((row) => [...row]);
+    newInputs[y][x] = count;
+    setUserInputs(newInputs);
+  };
+
   return (
     <div className={styles.container}>
-      <div
-      key = {`${y}-${x}`}
-      className={`cell ${cellClass(userInputs[y][x])}`}
-      onClick={() => clickHandler(x, y)}
-      <div className={styles.sampleCell} style={{ backgroundPosition: '-30px' }} />
-      {/* <button onClick={() => clickHandler()}> クリック </button> */}
-      </div>
+      {userInputs.map((row, y) => (
+        <div key={y} className={styles.row}>
+          {row.map((cell, x) => (
+            <div key={`${y}-${x}`} className={styles.cell} onClick={() => clickHandler(x, y)}>
+              {cell !== 0 ? cell : ''}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }

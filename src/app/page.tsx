664262b61c7afdb3020ff8660efd46476eceafe0
mui-1729 <s1f102503015@iniad.org>
2025-05-20 17:34:47 +0900

    再起

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ const generateBomb = (firstX: number, firstY: number) => {
   const newMap: number[][] = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0));
   let bombPlace = 0;
 
-  while (bombPlace <= 10) {
+  while (bombPlace < 10) {
     const x = Math.floor(Math.random() * 9);
     const y = Math.floor(Math.random() * 9);
 
@@ -43,19 +43,32 @@ const countBoardAround = (x: number, y: number, bombMap: number[][]) => {
   }, 0);
 };
 
+// 再起関数
+const connect = (x: number, y: number, bombMap: number[][], newInputs: number[][]) => {
+  if (x < 0 || x >= 9 || y < 0 || y >= 9 || newInputs[y][x] !== -1) return;
+
+  const count = countBoardAround(x, y, bombMap);
+  newInputs[y][x] = count;
+
+  if (count === 0) {
+    for (const [dx, dy] of directions) {
+      connect(x + dx, y + dy, bombMap, newInputs);
+    }
+  }
+};
+
 export default function Home() {
-  const [gameOver, setGameOver] = useState(false);
   const [isFirstClick, setIsFirstClick] = useState(true);
   const [userInputs, setUserInputs] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
   ]);
   const [bombMap, setBombMap] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -70,7 +83,7 @@ export default function Home() {
   ]);
 
   const clickHandler = (x: number, y: number) => {
-    if (gameOver === true || userInputs[y][x] !== 0) return;
+    if (userInputs[y][x] !== -1) return;
 
     if (isFirstClick === true) {
       const nextMap = generateBomb(x, y);
@@ -78,19 +91,20 @@ export default function Home() {
       setIsFirstClick(false);
       const count = countBoardAround(x, y, nextMap);
       const newInputs = userInputs.map((row) => [...row]);
+      connect(x, y, nextMap, newInputs);
       newInputs[y][x] = count;
       setUserInputs(newInputs);
       return;
     }
     if (bombMap[y][x] === 1) {
       alert('ゲームオーバー');
-      setGameOver(true);
       return;
     }
 
     const count = countBoardAround(x, y, bombMap);
     const newInputs = userInputs.map((row) => [...row]);
     newInputs[y][x] = count;
+    connect(x, y, bombMap, newInputs);
     setUserInputs(newInputs);
   };
 
@@ -98,11 +112,16 @@ export default function Home() {
     <div className={styles.container}>
       {userInputs.map((row, y) => (
         <div key={y} className={styles.row}>
-          {row.map((cell, x) => (
-            <div key={`${y}-${x}`} className={styles.cell} onClick={() => clickHandler(x, y)}>
-              {cell === 0 ? '黒' : cell}
-            </div>
-          ))}
+          {row.map((cell, x) => {
+            const suffix = cell === -1 ? 'hide' : `${cell}`;
+            const className = [styles.cell, styles[`cell${suffix}` as keyof typeof styles]].join(
+              ' ',
+            );
+
+            return (
+              <div key={`${y}-${x}`} className={className} onClick={() => clickHandler(x, y)} />
+            );
+          })}
         </div>
       ))}
     </div>

ba9486e3196894c4a01f5d65c5233423055e5865
mui-1729 <s1f102503015@iniad.org>
2025-05-24 17:34:55 +0900

    userInputs変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 
+type cellAction = 'none' | 'open' | 'flag' | null;
+
 const directions = [
   [1, 0],
   [1, 1],
@@ -22,6 +24,8 @@ const generateBomb = (firstX: number, firstY: number) => {
   while (bombPlace < 10) {
     const x = Math.floor(Math.random() * 9);
     const y = Math.floor(Math.random() * 9);
+
+    if (Math.abs(x - firstX) <= 1 && Math.abs(y - firstY) <= 1) continue;
 
     if ((x !== firstX || y !== firstY) && newMap[y][x] === 0) {
       newMap[y][x] = 1;
@@ -43,76 +47,96 @@ const countBoardAround = (x: number, y: number, bombMap: number[][]) => {
   }, 0);
 };
 
-// 再起関数
-const connect = (x: number, y: number, bombMap: number[][], newInputs: number[][]) => {
-  if (x < 0 || x >= 9 || y < 0 || y >= 9 || newInputs[y][x] !== -1) return;
+const calcBoard = (userInputs: cellAction[][], bombMap: number[][]): number[][] => {
+  const currentBoard = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => -1));
+  const alreadyVisited = Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => false));
 
-  const count = countBoardAround(x, y, bombMap);
-  newInputs[y][x] = count;
+  // 再起関数
+  const blankChain = (x: number, y: number) => {
+    if (x < 0 || x >= 9 || y < 0 || y >= 9 || alreadyVisited[y][x] === true) return;
 
-  if (count === 0) {
-    for (const [dx, dy] of directions) {
-      connect(x + dx, y + dy, bombMap, newInputs);
+    alreadyVisited[y][x] = true;
+
+    if (bombMap[y][x] === 1) return;
+
+    const count = countBoardAround(x, y, bombMap);
+    currentBoard[y][x] = count;
+
+    if (count === 0) {
+      for (const [dx, dy] of directions) {
+        blankChain(x + dx, y + dy);
+      }
+    }
+  };
+
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      if (userInputs[y][x] === 'open') {
+        if (bombMap[y][x] === 1) {
+          currentBoard[y][x] = -2;
+        } else {
+          blankChain(x, y);
+        }
+      }
     }
   }
+  return currentBoard;
 };
 
 export default function Home() {
-  const [isFirstClick, setIsFirstClick] = useState(true);
   const [seconds, setSeconds] = useState(0);
-  const [userInputs, setUserInputs] = useState(
-    Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => -1)),
+  const [userInputs, setUserInputs] = useState<cellAction[][]>(
+    Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => null)),
   );
-  const [bombMap, setBombMap] = useState(
-    Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0)),
-  );
+  const [bombMap, setBombMap] = useState<number[][] | null>(null);
 
-  const clickHandler = (x: number, y: number) => {
-    if (userInputs[y][x] !== -1) return;
-
-    if (isFirstClick === true) {
-      const nextMap = generateBomb(x, y);
-      setBombMap(nextMap);
-      setIsFirstClick(false);
-      const newInputs = userInputs.map((row) => [...row]);
-      connect(x, y, nextMap, newInputs);
-      setUserInputs(newInputs);
-      return;
-    }
-    if (bombMap[y][x] === 1) {
-      alert('ゲームオーバー');
-      return;
+  const LeftClickHandler = (x: number, y: number) => {
+    if (bombMap === null) {
+      setBombMap(generateBomb(x, y));
     }
 
-    const newInputs = userInputs.map((row) => [...row]);
-    connect(x, y, bombMap, newInputs);
-    setUserInputs(newInputs);
+    setUserInputs((prev) => {
+      const newInputs = prev.map((row) => [...row]);
+      newInputs[y][x] = 'open';
+      return newInputs;
+    });
   };
 
-  const Timer = () => {
-    useEffect(() => {
-      const timer = setInterval(() => {
-        setSeconds((prev) => prev + 1);
-      }, 1000);
+  const RightClickHandler = (event: React.MouseEvent, x: number, y: number) => {
+    event.preventDefault();
 
-      return () => clearInterval(timer);
-    }, []);
+    setUserInputs((prev) => {
+      const newInputs = prev.map((row) => [...row]);
+      const states: cellAction[] = [null, 'flag', 'none'];
+      const currentIndex = states.indexOf(newInputs[y][x]);
+      const nextIndex = (currentIndex + 1) % states.length;
+      newInputs[y][x] = states[nextIndex];
+      return newInputs;
+    });
   };
 
   const handleReset = () => {
-    {
-      setUserInputs(Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => -1)));
-      setBombMap(Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0)));
-      setIsFirstClick(true);
+    setUserInputs(Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => null)));
+    setBombMap(null);
+    setSeconds(0);
+  };
+
+  useEffect(() => {
+    if (bombMap === null) {
       setSeconds(0);
     }
-  };
-  Timer();
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [bombMap]);
+
+  const board = bombMap ? calcBoard(userInputs, bombMap) : null;
 
   return (
     <div className={styles.container}>
       <div className={styles.timer}>
-        Time: {seconds}秒<div onClick={handleReset}>リセット</div>
+        Time: {seconds}秒<button onClick={handleReset}>リセット</button>
       </div>
       {userInputs.map((row, y) => (
         <div key={y} className={styles.row}>
